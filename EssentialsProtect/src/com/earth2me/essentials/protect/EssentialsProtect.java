e08abd3af93f0e258498b6d86b17bcd831a93ede
XMas <xstmas@users.noreply.github.com>
2020-10-01 08:27:19 +0300

    Revert "A few lots of changes to make this plugin better for my network"
    This reverts commit 5d7d01676c8d0f5ea2f37d32cc5aef0b78b2ec3c.

---
+++ EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtect.java
@@ -0,0 +1,109 @@
+package com.earth2me.essentials.protect;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class EssentialsProtect extends JavaPlugin implements IProtect {
+    private static final Logger LOGGER = Logger.getLogger("Minecraft");
+    private final Map<ProtectConfig, Boolean> settingsBoolean = new EnumMap<ProtectConfig, Boolean>(ProtectConfig.class);
+    private final Map<ProtectConfig, String> settingsString = new EnumMap<ProtectConfig, String>(ProtectConfig.class);
+    private final Map<ProtectConfig, List<Integer>> settingsList = new EnumMap<ProtectConfig, List<Integer>>(ProtectConfig.class);
+    private EssentialsConnect ess = null;
+
+    private final EmergencyListener emListener = new EmergencyListener(this);
+
+    @Override
+    public void onEnable() {
+        final PluginManager pm = this.getServer().getPluginManager();
+        final Plugin essPlugin = pm.getPlugin("Essentials");
+        if (essPlugin == null || !essPlugin.isEnabled()) {
+            enableEmergencyMode(pm);
+            return;
+        }
+        
+        initialize(pm, essPlugin);
+    }
+
+    private void initialize(final PluginManager pm, final Plugin essPlugin) {
+        LOGGER.log(Level.INFO, "Continuing to enable Protect.");
+        ess = new EssentialsConnect(essPlugin, this);
+
+        final EssentialsProtectBlockListener blockListener = new EssentialsProtectBlockListener(this);
+        pm.registerEvents(blockListener, this);
+
+        final EssentialsProtectEntityListener entityListener = new EssentialsProtectEntityListener(this);
+        pm.registerEvents(entityListener, this);
+
+        final EssentialsProtectWeatherListener weatherListener = new EssentialsProtectWeatherListener(this);
+        pm.registerEvents(weatherListener, this);
+    }
+
+    private void enableEmergencyMode(final PluginManager pm) {
+        pm.registerEvents(emListener, this);
+
+        for (Player player : getServer().getOnlinePlayers()) {
+            player.sendMessage("Essentials Protect is in emergency mode. Check your log for errors.");
+        }
+        LOGGER.log(Level.SEVERE, "Essentials not installed or failed to load. Essentials Protect is in emergency mode now.");
+    }
+
+    void disableEmergencyMode() {
+        final PluginManager pm = this.getServer().getPluginManager();
+        final Plugin essPlugin = pm.getPlugin("Essentials");
+        if (essPlugin == null || !essPlugin.isEnabled()) {
+            LOGGER.log(Level.SEVERE, "Tried to disable emergency mode, but Essentials still isn't enabled!");
+            return;
+        }
+
+        HandlerList.unregisterAll(emListener);
+
+        for (Player player : getServer().getOnlinePlayers()) {
+            player.sendMessage("Essentials Protect is no longer in emergency mode.");
+        }
+        LOGGER.log(Level.SEVERE, "Essentials was loaded late! Essentials Protect is no longer in emergency mode.");
+
+        initialize(pm, essPlugin);
+    }
+
+    @Override
+    public EssentialsConnect getEssentialsConnect() {
+        return ess;
+    }
+
+    @Override
+    public Map<ProtectConfig, Boolean> getSettingsBoolean() {
+        return settingsBoolean;
+    }
+
+    @Override
+    public Map<ProtectConfig, String> getSettingsString() {
+        return settingsString;
+    }
+
+    @Override
+    public Map<ProtectConfig, List<Integer>> getSettingsList() {
+        return settingsList;
+    }
+
+    @Override
+    public boolean getSettingBool(final ProtectConfig protectConfig) {
+        final Boolean bool = settingsBoolean.get(protectConfig);
+        return bool == null ? protectConfig.getDefaultValueBoolean() : bool;
+    }
+
+    @Override
+    public String getSettingString(final ProtectConfig protectConfig) {
+        final String str = settingsString.get(protectConfig);
+        return str == null ? protectConfig.getDefaultValueString() : str;
+    }
+}
